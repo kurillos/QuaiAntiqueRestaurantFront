fb7df24d3bbdef5362d346d5a3aa16ab94570371
kurillos <bocage.cyril@gmail.com>
2024-04-22 14:43:02 +0200

    Ajout de la Galerie

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -2,7 +2,10 @@ import Route from "./Route.js";
 
 //définir ici vos routes
 export const allRoutes = [
-    new Route("/", "Accueil", "/pages/home.html"),];
+    new Route("/", "Accueil", "/pages/home.html"),
+    new Route("/galerie", "La Galerie", "/pages/galerie.html"),
+
+];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "Quai Antique";
