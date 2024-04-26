8cf034dcd5f368d25cfa3633df47546543a9837c
kurillos <bocage.cyril@gmail.com>
2024-04-26 13:26:18 +0200

    Ajout de l'authentification

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -4,6 +4,10 @@ import Route from "./Route.js";
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
     new Route("/galerie", "La Galerie", "/pages/galerie.html"),
+    new Route("/signin", "Connexion", "/pages/auth/signin.html"),
+    new Route("/signup", "Inscription", "/pages/auth/signup.html"),
+    new Route("/account", "Mon compte", "/pages/auth/account.html"),
+    new Route("/editPassword", "Changement de Mot de Passe", "/pages/auth/editPassword.html"),
 
 ];
 
