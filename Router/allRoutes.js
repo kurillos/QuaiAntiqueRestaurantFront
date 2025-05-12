2f9e5d193beeb08c4718470cfef99b8d7af2a4d5
kurillos <bocage.cyril@gmail.com>
2025-05-12 12:37:18 +0200

    Ajout système routage

---
+++ Router/allRoutes.js
@@ -0,0 +1,13 @@
+import Route from "./Route.js";
+
+//Définir ici vos routes
+
+export const routes = [
+
+    new Route("/", "Accueil", "/pages/home.html"),
+    
+];
+
+//Le titre s'affiche comme ceci : Route.titre - websitename
+
+export const websiteName = "Quai Antique";
