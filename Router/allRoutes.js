9807f78a32943200f67ba97896da7b6759fa4ec5
kurillos <bocage.cyril@gmail.com>
2025-05-14 10:45:13 +0200

    ajout des option authentifier

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -6,6 +6,10 @@ export const routes = [
 
     new Route("/", "Accueil", "/pages/home.html"),
     new Route("/galerie", "La galerie", "/pages/galerie.html"),
+    new Route("/signin", "Connexion", "/pages/auth/signin.html"),
+    new Route("/signup", "Inscription", "/pages/auth/signup.html"),
+    new Route("/account", "Mon compte", "/pages/auth/account.html"),
+    new Route("/editPassword", "Changement de mot de passe", "/pages/auth/editPassword.html"),
     
 ];
 
