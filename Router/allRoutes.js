830c1242ab0e6a37474d006ae4ccbbef60067efe
kurillos <bocage.cyril@gmail.com>
2025-05-14 11:45:36 +0200

    Gestion de l'affichage des réservations

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -10,6 +10,7 @@ export const routes = [
     new Route("/signup", "Inscription", "/pages/auth/signup.html"),
     new Route("/account", "Mon compte", "/pages/auth/account.html"),
     new Route("/editPassword", "Changement de mot de passe", "/pages/auth/editPassword.html"),
+    new Route("/allResa", "Vos réservations", "/pages/reservations/allResa.html"),
     
 ];
 
